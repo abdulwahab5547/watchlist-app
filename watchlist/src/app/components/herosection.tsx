5040e99d5e2d6012a3afc0f4287ecaf25726067f
Abdul Wahab Asif <abdulwahab5547@gmail.com>
2024-11-21 21:54:25 +0500

    mobile friendliness

--- watchlist/src/app/components/herosection.tsx
+++ watchlist/src/app/components/herosection.tsx
@@ -112,49 +112,64 @@ function HeroSection() {
           {/* Movie Grid */}
           <div className="grid md:grid-cols-8 grid-cols-2 gap-3 relative">
             {section.movies.slice(0, 24).map((movie) => (
-              <div
-                key={movie._id}
-                className="relative rounded-lg group transition-transform duration-300 hover:scale-110"
-              >
-                <Image
-                  src={movie.poster_path}
-                  alt="movie poster"
-                  width={300}
-                  height={450}
-                  className="img-fluid rounded-lg transition-opacity duration-300 group-hover:opacity-30"
-                />
-
-                <div className="absolute inset-0 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  <div className="px-6">
-                    <Link href={`/movie-details/${movie._id}`}>
-                      <p className="text font-bold text-sm">{movie.title}</p>
-                    </Link>
-
-                    {/* Display Date Only If Valid */}
-                    {formatDate(movie.release_date) && (
-                      <p className="text-sm flex items-center pt-4 text-xs">
-                        <i className="fa-solid fa-calendar text-orange pr-1"></i>
-                        {formatDate(movie.release_date)}
-                      </p>
-                    )}
-
-                    <div className="pt-6">
-                      <Link href={`/movie-details/${movie._id}`}>
-                        <div className="hover:text-orange text-sm">
-                          View Info
-                          <i className="pl-2 fa-solid fa-circle-info"></i>
-                        </div>
-                      </Link>
-                    </div>
+              <div>
+                <Link href={`/movie-details/${movie._id}`} className="block md:hidden">
+                  <div
+                    key={movie._id}
+                    className="relative rounded-lg group transition-transform duration-300 hover:scale-110"
+                  >
+                    <Image
+                      src={movie.poster_path}
+                      alt="movie poster"
+                      width={300}
+                      height={450}
+                      className="img-fluid rounded-lg transition-opacity duration-300 group-hover:opacity-30"
+                    />
                   </div>
-                  <div className="absolute top-2 right-2">
-                    <i
-                      className="fa-regular fa-bookmark text-lightGray hover:text-white text-sm hover:cursor-pointer"
-                      onClick={() => handleBookmarkClick(movie)}
-                    ></i>
+                </Link>
+                <div className="hidden md:block relative rounded-lg group transition-transform duration-300 hover:scale-110">
+                  <Image
+                    src={movie.poster_path}
+                    alt="movie poster"
+                    width={300}
+                    height={450}
+                    className="img-fluid rounded-lg transition-opacity duration-300 group-hover:opacity-30"
+                  />
+
+                  <div className="absolute inset-0 flex flex-col justify-center items-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <div className="px-6">
+                      <Link href={`/movie-details/${movie._id}`}>
+                        <p className="text font-bold text-sm">{movie.title}</p>
+                      </Link>
+
+                      {/* Display Date Only If Valid */}
+                      {formatDate(movie.release_date) && (
+                        <p className="text-sm flex items-center pt-4 text-xs">
+                          <i className="fa-solid fa-calendar text-orange pr-1"></i>
+                          {formatDate(movie.release_date)}
+                        </p>
+                      )}
+
+                      <div className="pt-6">
+                        <Link href={`/movie-details/${movie._id}`}>
+                          <div className="hover:text-orange text-sm">
+                            View Info
+                            <i className="pl-2 fa-solid fa-circle-info"></i>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="absolute top-2 right-2">
+                      <i
+                        className="fa-regular fa-bookmark text-lightGray hover:text-white text-sm hover:cursor-pointer"
+                        onClick={() => handleBookmarkClick(movie)}
+                      ></i>
+                    </div>
                   </div>
                 </div>
               </div>
+              
+              
             ))}
           </div>
         </div>
